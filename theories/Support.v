1fdeb49f26afe39fc34ada17a60f94477f4755d8
Max Ole Elliger <ole.elliger@fau.de>
2024-07-25 11:20:02 +0200

    chg: Updated the definition of states
    states are now functions to bool instead of Prop.

--- theories/Support.v
+++ theories/Support.v
@@ -3,24 +3,19 @@ Definition atoms := nat.
 Class Model :=
   {
     possible_worlds : Type;
+    some_world : possible_worlds;
     truth_value : possible_worlds -> atoms -> bool
   }.
 
-Definition state `{Model} := possible_worlds -> Prop.
+Definition state `{Model} := possible_worlds -> bool.
 
-Definition empty_state `{Model} : state := fun _ => False.
+Definition substate `{Model} (t s : state) :=
+  forall w,
+    t w = true ->
+    s w = true.
+
+Definition empty_state `{Model} : state := fun _ => false.
 
 Definition consistent `{Model} (s : state) : Prop :=
   exists w,
-    s w.
-
-Lemma consistent_state_not_empty `{Model} :
-  forall s,
-    consistent s ->
-    s <> empty_state.
-Proof.
-  intros s H1 H2.
-  subst.
-  destruct H1 as [w H1].
-  assumption.
-Qed.
+    s w = true.
