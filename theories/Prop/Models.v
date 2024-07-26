adabdff5ef6343dff3f8e512286802bb1b81853e
Max Ole Elliger <ole.elliger@fau.de>
2024-07-26 10:12:28 +0200

    new: `satisfies`-relation for LPC
    Added `worlds_deceq`
    Added `singleton` states
    Added `satisfies`

--- theories/Prop/Models.v
+++ theories/Prop/Models.v
@@ -3,6 +3,7 @@ Definition atoms := nat.
 Class Model :=
   {
     worlds : Type;
+    worlds_deceq : forall (w1 w2 : worlds), {w1 = w2} + {w1 <> w2};
     truth_value : worlds -> atoms -> bool
   }.
 
@@ -18,3 +19,6 @@ Definition empty_state `{Model} : state := fun _ => false.
 Definition consistent `{Model} (s : state) : Prop :=
   exists w,
     s w = true.
+
+Definition singleton `{Model} (w : worlds) : state :=
+  fun w' => if worlds_deceq w' w then true else false.
