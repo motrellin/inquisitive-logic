443cde68852405c29afe65dd1131e09eebfbc0fe
Max Ole Elliger <ole.elliger@fau.de>
2024-07-27 00:14:18 +0200

    new: Added `restricted_state`

--- theories/Prop/LP.v
+++ theories/Prop/LP.v
@@ -343,3 +343,17 @@ Proof.
     intros [w1] [w2].
     apply truth_value_proper.
 Defined.
+
+Definition restricted_state `{Model} (s t : state) : @state (restricted_Model s).
+Proof.
+  unshelve econstructor.
+  -
+    intros [w].
+    apply t.
+    exact w.
+  -
+    intros [w1] [w2] H1.
+    simpl in *.
+    rewrite H1.
+    reflexivity.
+Defined.
