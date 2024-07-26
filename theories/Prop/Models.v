e983a194fc781c52e2dc691a506ce6f6ce513850
Max Ole Elliger <ole.elliger@fau.de>
2024-07-26 12:03:18 +0200

    new: Some Instances
    Defined `state_equiv`
    Declared `state_equiv` as an `Equivalence`
    Declared `Proper (state_equiv ==> state_equiv ==> iff) substate`
    Declared `substate` as a reflexive relation

--- theories/Prop/Models.v
+++ theories/Prop/Models.v
@@ -1,3 +1,5 @@
+From Coq Require Export RelationClasses Morphisms Setoid.
+
 Definition atoms := nat.
 
 Class Model :=
@@ -8,11 +10,57 @@ Class Model :=
   }.
 
 Definition state `{Model} := worlds -> bool.
+Definition state_equiv `{Model} (s1 s2 : state) : Prop :=
+  forall w, s1 w = s2 w.
+
+Instance state_equiv_equiv : forall `{Model}, Equivalence state_equiv.
+Proof.
+  split.
+  -
+    intros s w; reflexivity.
+  -
+    intros s t H1 w.
+    symmetry.
+    exact (H1 w).
+  -
+    intros s t u H1 H2 w.
+    rewrite H1.
+    rewrite H2.
+    reflexivity.
+Qed.
 
 Definition substate `{Model} (t s : state) :=
   forall w,
     t w = true ->
     s w = true.
+
+#[export] Instance substate_proper :
+  forall `{Model},
+    Proper (state_equiv ==> state_equiv ==> iff) substate.
+Proof.
+  intros M s1 s2 H1 t1 t2 H2.
+  unfold state_equiv, substate in *.
+  split.
+  -
+    intros H3 w H4.
+    rewrite <- H2.
+    apply H3.
+    rewrite H1.
+    exact H4.
+  -
+    intros H3 w H4.
+    rewrite H2.
+    apply H3.
+    rewrite <- H1.
+    exact H4.
+Qed.
+  
+
+Instance substate_refl : forall `{Model}, Reflexive substate.
+Proof.
+  intros M s w H1.
+  assumption.
+Qed.
 
 Definition empty_state `{Model} : state := fun _ => false.
 
