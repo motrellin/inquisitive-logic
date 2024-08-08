7856d5ed6a34e0d6fed399d42274780c40f80606
Max Ole Elliger <ole.elliger@fau.de>
2024-08-08 16:43:24 +0200

    chg: Redefined list_func

--- theories/ListFunc.v
+++ theories/ListFunc.v
@@ -1,39 +1,17 @@
 From Coq Require Export List.
 
-Section list_func_helper.
-
-  Context {X : Type}.
-  Context {Y : Type}.
-  Context (x : X).
-  Context (xs' : list X).
-  Context (R : list Y -> list (list (X*Y))).
-
-  Definition list_func_helper : list Y -> list (list (X*Y)) :=
-    list_rect
-    _
-    (map (fun y => (x,y) :: nil))
-    (fun _ _ _ ys =>
-      flat_map
-      (
-        fun f =>
-        map
-        (
-          fun y =>
-          (x,y) :: f
-        )
-        ys
-      )
-      (R ys)
-    )
-    xs'.
-
-End list_func_helper.
-
 Definition list_func {X Y} : list X -> list Y -> list (list (X*Y)) :=
   list_rect
   _
-  (fun ys => nil)
-  list_func_helper.
+  (fun ys => nil :: nil)
+  (fun x xs' R =>
+    list_rect
+    _
+    nil
+    (fun y ys' _ =>
+      flat_map (fun xys => map (fun y => (x,y) :: xys) (y :: ys')) (R (y :: ys'))
+    )
+  ).
 
 Compute (list_func (1 :: 2 :: nil) (3 :: 4 :: nil)).
 Compute length (list_func (1 :: 2 :: 3 :: nil) (4 :: 5 :: 6 :: nil)).
