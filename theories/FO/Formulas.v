9a652c16c83aa009d1fcc887882fd871eb694d43
Max Ole Elliger <ole.elliger@fau.de>
2024-10-12 12:10:18 +0200

    new: Autosubst for FO-formulas
    Autosubst provides support for binding signatures. To use it, our
    signature had to be updated: The `Bot`-operator now needs a variable as
    argument which isn't used further.
    
    Possible improvements:
    - Find a workaround to ignore this useless argument for `Bot`.
    - Improve documentation about the use of Autosubst.

--- theories/FO/Formulas.v
+++ theories/FO/Formulas.v
@@ -17,6 +17,20 @@ Inductive term `{Signature} :=
   | Var : var -> term
   | Func : forall (f : FSymb), (FAri f -> term) -> term.
 
+(* Autosubst-stuff about terms *)
+
+Instance Ids_term `{Signature} : Ids term.
+Proof. derive. Defined.
+
+Instance Rename_term `{Signature} : Rename term.
+Proof. derive. Defined.
+
+Instance Subst_term `{Signature} : Subst term.
+Proof. derive. Defined.
+
+Instance SubstLemmas_term `{Signature} : SubstLemmas term.
+Proof. derive. Qed.
+
 (* syntax of first-order formulas including inquisitive operators *)
 
 Inductive form `{Signature} :=
@@ -24,7 +38,7 @@ Inductive form `{Signature} :=
   | Pred : forall (p : PSymb), (PAri p -> term) -> form
 
   (* propositional connectives *)
-  | Bot : form
+  | Bot (v : var) : form
   | Impl : form -> form -> form
   | Conj : form -> form -> form
   | Idisj : form -> form -> form
@@ -32,3 +46,26 @@ Inductive form `{Signature} :=
   (* first-order connectives *)
   | Forall : {bind term in form} -> form
   | Iexists : {bind term in form} -> form.
+
+(* Autosubst-stuff about formulas *)
+
+Instance HSubst_form `{Signature} : HSubst term form.
+Proof. derive. Defined.
+
+Instance Ids_form `{Signature} : Ids form.
+Proof. derive. Defined.
+
+Instance Rename_form `{Signature} : Rename form.
+Proof. derive. Defined.
+
+Instance Subst_form `{Signature} : Subst form.
+Proof. derive. Defined.
+
+Instance HSubstLemmas_form `{Signature} : HSubstLemmas term form.
+Proof. derive. Qed.
+
+Instance SubstHSubstComp_term_form `{Signature} : SubstHSubstComp term form.
+Proof. derive. Qed.
+
+Instance SubstLemmas_form `{Signature} : SubstLemmas form.
+Proof. derive. Qed.
