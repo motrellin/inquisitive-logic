c019c775e406d7d2f7616811d18c9f2bf842e67c
Max Ole Elliger <ole.elliger@fau.de>
2024-11-02 18:58:45 +0100

    new: Started working on Casari
    - Defined a signature with exact one predicate (of arity 1) and no
      function symbols.
    - Provided a proof of double negated atomic Casari in Coq's `Prop`.
    - Defined the Casari Scheme as a `form`ula in our syntax.
    - Proved, that Casari Scheme with double negated atoms holds in every
      world.

---
+++ theories/FO/Casari.v
@@ -0,0 +1,133 @@
+From InqLog Require Export Support.
+
+Instance signature : Signature :=
+  {|
+    PSymb := unit;
+    PAri := fun p => match p with tt => unit end;
+    FSymb := Empty_set;
+    FAri := fun f => match f with end;
+    rigid := fun _ => true
+  |}.
+
+Definition Pred' (t : term) := Pred tt (fun arg => t).
+
+Theorem CasariProp (P : var -> Prop) :
+  (
+    forall x,
+      (
+        (P x)
+        ->
+        (forall x, ~ ~ P x)
+      )
+      ->
+      (forall x, ~ ~ P x)
+  )
+  ->
+  (
+    forall x, ~ ~ P x
+  ).
+Proof.
+  firstorder.
+Qed.
+
+Print Assumptions CasariProp.
+
+Definition Casari (phi : var -> form) : form :=
+  Impl
+  (
+    Forall
+    (
+      Impl
+      (
+        Impl
+        (
+          phi 0
+        )
+        (
+          Forall (phi 0)
+        )
+      )
+      (
+        Forall (phi 0)
+      )
+    )
+  )
+  (
+    Forall (phi 0)
+  ).
+
+Definition CasariDNA : form :=
+  Casari (fun x => Neg (Neg (Pred' (Var x)))).
+
+Definition CasariAtomic : form :=
+  Casari (fun x => Pred' (Var x)).
+
+Theorem truth_CasariDNA `{@Model signature} :
+  forall w a,
+    support CasariDNA (singleton w) a.
+Proof.
+  intros w1 a.
+
+  intros s2 H1 H2.
+
+  apply substate_singleton in H1 as [H1|H1]; rewrite H1 in *; clear s2 H1.
+  now apply empty_state_property.
+
+  intros i1.
+  rewrite support_Neg.
+  intros [s3 [[w2 H3] [H4 H5]]].
+
+  enough (
+    exists s,
+      support (Forall (Neg (Neg (Pred' (Var 0))))) s (i1 .: a) /\
+      substate s3 s
+  ) as [s4 [H6 H7]].
+  {
+    specialize (H6 i1). fold support in H6.
+
+    rewrite support_Neg in H6.
+    apply H6.
+    exists s3.
+    firstorder.
+  }
+
+  specialize (H2 i1). fold support in H2.
+  remember (Neg (Neg (Pred' (Var 0)))) as phi1 eqn:eq1.
+  remember (Forall phi1) as phi2 eqn:eq2.
+  remember (Impl phi1 phi2) as phi3 eqn:eq3.
+
+
+  eexists.
+  split.
+
+  apply H2. exact H4.
+
+  fold support.
+  subst phi3.
+
+  apply substate_singleton in H4 as [H4|H4]; rewrite H4 in *; clear s3 H4.
+  now apply empty_state_property.
+
+  intros s4 H6 H7.
+
+  apply substate_singleton in H6 as [H6|H6]; rewrite H6 in *; clear s4 H6.
+  now apply empty_state_property.
+
+  exfalso.
+
+  rewrite eq1 in H7.
+  rewrite support_Neg in H7.
+  apply H7.
+
+  eexists.
+  repeat split.
+  -
+    eexists.
+    exact H3.
+  -
+    reflexivity.
+  -
+    exact H5.
+  -
+    reflexivity.
+Qed.
