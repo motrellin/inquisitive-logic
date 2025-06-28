36706ccfaadd63555ca99adeceba9b71802d59a6
Max Ole Elliger <ole.elliger@fau.de>
2025-06-28 23:39:17 +0200

    chg: Cleaned up examples/FO/CD.v

--- examples/FO/CD.v
+++ examples/FO/CD.v
@@ -2,8 +2,8 @@ From InqLog.FO Require Import Seq.
 
 Definition CD `{Signature} (phi psi : form) : form :=
   <{
-    (Forall <{(hsubst (ren (+1)) phi) \\/ psi}>) ->
-    phi \\/(Forall psi)
+    (Forall <{phi \\/ (hsubst (ren (+1)) psi)}>) ->
+    (Forall phi) \\/ psi
   }>.
 
 Theorem support_valid_CD `{Signature} :
@@ -15,61 +15,55 @@ Proof.
   intros t H1 H2.
   destruct (
     classic (
-      t,a |= phi
+      t,a |= psi
     )
   ) as [H3|H3].
   -
-    left.
+    right.
     exact H3.
   -
-    right.
+    left.
     intros i.
     rewrite support_Forall in H2.
     specialize (H2 i).
     destruct H2 as [H2|H2].
     +
+      exact H2.
+    +
       exfalso.
       apply H3.
       apply support_hsubst_var in H2.
       apply H2.
-    +
-      exact H2.
 Qed.
 
-Example Seq_ex_3 `{Signature} :
+Theorem Seq_CD `{Signature} :
   forall ns phi psi,
-    Seq
-    ((pair ns <{(forall phi) \\/ psi}>) :: nil)
-    ((pair ns (Forall (Idisj phi psi.|[ren (+1)]))) :: nil).
+    Seq nil ((pair ns (CD phi psi)) :: nil).
 Proof with (
   try (left; simpl; reflexivity);
   try (right; left; simpl; reflexivity);
   try easy
 ).
   intros ns phi psi.
+  unfold CD.
+  eapply Seq_Impl_r...
+  intros ns' H1.
+  eapply Seq_Idisj_r...
   eapply Seq_Forall_r...
+  eapply Seq_Forall_l with (t := Var 0)...
   eapply Seq_Idisj_l...
-  all: eapply Seq_Idisj_r...
-  -
-    eapply Seq_Forall_l with (t := Var 0)...
-    eapply Seq_persistency.
-    {
-      apply InS_cons_I_hd.
-      reflexivity.
-    }
-    {
-      apply InS_cons_I_hd.
-      f_equiv.
-      rewrite <- hsubst_id' with
-        (phi := phi)
-        at 2.
-      rewrite hsubst_comp'.
-      apply hsubst_Proper; try reflexivity.
-      intros [|]; reflexivity.
-    }
-    {
-      reflexivity.
-    }
   -
     eapply Seq_persistency...
+    apply InS_cons_I_hd.
+    rewrite hsubst_comp'.
+    f_equiv.
+    rewrite <- hsubst_id' with (phi := phi) at 2.
+    apply hsubst_Proper; try reflexivity.
+    intros [|x']; reflexivity.
+  -
+    eapply Seq_persistency...
+    do 2 apply InS_cons_I_tl.
+    apply InS_cons_I_hd.
+    repeat rewrite hsubst_comp'.
+    f_equiv.
 Qed.
